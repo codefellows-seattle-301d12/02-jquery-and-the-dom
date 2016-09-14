24c7e3f022b9066cf85adc574198742ce7f5e068
pasaunders <kaorti@gmail.com>
2016-09-13 17:56:37 -0700

    everything seems to work. articles print properly, with embedded pictures and everything.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -14,9 +14,10 @@ Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
   $newArticle.find('h1').text(this.title);
-  $newArticle.find('a').text(this.author);
+  $newArticle.find('address a').attr('href', this.authorUrl);
+  $newArticle.find('address a').text(this.author);
   $newArticle.find('time').text(this.publishedOn);
-  $newArticle.find('section.article-body').text(this.body);
+  $newArticle.find('section.article-body').append(this.body);
   // TODO: Use jQuery to fill in the rest of the current template clone
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   // TODO: This cloned article is no longer a template. We need to remove its class.
