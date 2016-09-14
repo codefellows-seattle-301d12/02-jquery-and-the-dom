2557ca47d26c3aa7464516bd6aaf92ebbafbe9d9
ackenepah <ackenepah@gmail.com>
2016-09-14 06:03:26 -0700

    attempted to clear out the todo list. still some more things to do

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -2,15 +2,29 @@ var articles = [];
 
 function Article (options) {
   this.title = options.title;
+  
   // TODO: Use the js object passed in to complete the constructor function.
+  this.category = options.category;
+  this.author = options.author;
+  this.authorUrl = options.authorUrl;
+  this.publishedOn = options.publishedOn;
+  this.body = options.body;
 };
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
+
   // TODO: Use jQuery to fill in the rest of the current template clone
+  $newArtlicle.find('h1').text(this.title);
+  $newArticle.find('address').text(this.author);
+  $newArticle.find().text(this.authorUrl);
+  $newArticle.attr('article-body').text(this.body);
+
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+
   // TODO: This cloned article is no longer a template. We need to remove its class.
+  $('article').removeClass('template');
   return $newArticle;
 };
 
